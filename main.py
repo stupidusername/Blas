6e5453abc3b30d8b05bdf205519c305ac0851363
nico <nicolas.p.suarez@gmail.com>
2017-08-27 15:18:53 -0300

    added music categories response

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import abort, Flask, request
+import inspect
+import json
+import os
 app = Flask('Blas')
 
 
 @app.route('/api/get-radios')
 def get_radios():
-    return ''
+    music_category_folders = list_folders(get_folder('music'))
+    return json.dumps(build_music_categories(music_category_folders))
 
 
 @app.route('/api/get-radio-songs')
@@ -23,3 +27,30 @@ def get_audio_message():
     if not key:
         abort(400)
     return key
+
+
+def get_files_root_folder():
+    try:
+        script_dir = os.path.dirname(
+            os.path.abspath(inspect.getfile(inspect.currentframe())))
+        config_file = open(script_dir + '/config.json')
+        config_file.seek(0)
+        config = json.load(config_file)
+        files_root_folder = config['files_root_folder'].strip('/')
+        return files_root_folder
+    except:
+        abort(500)
+
+
+def get_folder(folder):
+    folder = folder.strip('/')
+    return get_files_root_folder() + '/' + folder
+
+
+def list_folders(path):
+    return [name for name in os.listdir(path)
+            if os.path.isdir(os.path.join(path, name))]
+
+
+def build_music_categories(categories):
+    return [{'id': idx, 'title': title} for idx, title in enumerate(categories)]
